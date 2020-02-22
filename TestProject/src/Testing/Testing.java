3617df3d157b545283bc33cc3e0e8bee19a921cd
Kapil <Kapil@192.168.178.48>
2020-02-23 00:03:44 +0100

    updated

--- TestProject/src/Testing/Testing.java
+++ TestProject/src/Testing/Testing.java
@@ -5,6 +5,8 @@ public class Testing {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello there");
+		System.out.print("Just checking if this working"); 
+		System.out.println("Right here");
 	}
 
 }
